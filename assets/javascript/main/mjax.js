164af59d4dccf167b379ac994dcfd100a1921aa6
Sean Jezewski <seanwjezewski@gmail.com>
2012-10-10 09:20:28 -0700

    Clean up js

--- assets/javascript/main/mjax.js
+++ assets/javascript/main/mjax.js
@@ -5,20 +5,6 @@
 
 $(document).ready(
   function() {
-    console.log("heyasdfsqdF")
-
-//  $(document).pjax("a").on('pjax:error', function(e, xhr, err) {
-
-/*  $("a").pjax().on('pjax:error', function(e, xhr, err) {
-      console.log('Something went wrong: ' + err);
-  })*/
-
-/*    $("[data-pjax]").each(
-      function(elem) {
-        
-      }
-    );
-*/
 
   $(document).pjax('a[data-pjax]');
 
